a8223421db3df6d11fda16401ce57402009623f9
GibsCat <>
2023-11-10 08:24:34 -0700

    versionSemifinal

--- src/mx/itson/catrina/ui/Main.java
+++ src/mx/itson/catrina/ui/Main.java
@@ -45,7 +45,6 @@ public class Main extends javax.swing.JFrame {
         btnSeleccione = new javax.swing.JButton();
         jTextField2 = new javax.swing.JTextField();
         txtNombre = new javax.swing.JTextField();
-        txtTipoCuenta = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblDatosCliente = new javax.swing.JTable();
         jScrollPane2 = new javax.swing.JScrollPane();
@@ -61,6 +60,7 @@ public class Main extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         lblImagen = new javax.swing.JLabel();
         lblEscudo = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Estado de cuenta");
@@ -104,17 +104,13 @@ public class Main extends javax.swing.JFrame {
 
         jTextField2.setEditable(false);
         jTextField2.setBackground(new java.awt.Color(204, 204, 255));
-        jTextField2.setFont(new java.awt.Font("Dialog", 0, 24)); // NOI18N
+        jTextField2.setFont(new java.awt.Font("Gill Sans MT Condensed", 0, 24)); // NOI18N
         jTextField2.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         jTextField2.setText("ESTADO DE CUENTA");
 
         txtNombre.setEditable(false);
         txtNombre.setBackground(new java.awt.Color(255, 204, 255));
         txtNombre.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
-
-        txtTipoCuenta.setEditable(false);
-        txtTipoCuenta.setBackground(new java.awt.Color(255, 204, 255));
-        txtTipoCuenta.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
 
         tblDatosCliente.setBackground(new java.awt.Color(204, 204, 204));
         tblDatosCliente.setModel(new javax.swing.table.DefaultTableModel(
@@ -261,6 +257,8 @@ public class Main extends javax.swing.JFrame {
 
         lblEscudo.setBackground(new java.awt.Color(218, 233, 239));
 
+        jLabel4.setText("jLabel4");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -284,8 +282,8 @@ public class Main extends javax.swing.JFrame {
                             .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 286, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txtTipoCuenta, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel4)))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(cboMes, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 162, Short.MAX_VALUE)
@@ -325,7 +323,7 @@ public class Main extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtTipoCuenta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel4))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -405,7 +403,6 @@ public class Main extends javax.swing.JFrame {
                 modelo.addRow(new Object[]{cuenta.getCustomer().getAddress()});
                 modelo.addRow(new Object[]{cuenta.getCustomer().getCity()});
                 modelo.addRow(new Object[]{"C.P. " + cuenta.getCustomer().getZip_code()});
-                txtTipoCuenta.setText(cuenta.getProduct());
 
                 modelo1.addRow(new Object[]{"CUENTA", String.format("%25s", cuenta.getAccount())});
                 modelo1.addRow(new Object[]{"CLABE", String.format("%16s", cuenta.getClabe())});
@@ -567,6 +564,7 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
@@ -584,7 +582,6 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JTextField txtArchivo;
     private javax.swing.JTextField txtNombre;
     private javax.swing.JTextField txtSaldoFinal;
-    private javax.swing.JTextField txtTipoCuenta;
     // End of variables declaration//GEN-END:variables
 
 }
